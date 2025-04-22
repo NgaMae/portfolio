3ee90d0b1ad87754842725a9df70d9f0c2ffe2f7
San Min Aung <ngamae2@gmail.com>
2025-04-22 07:43:23 +0630

    Header and Hero sections are initialized

---
+++ src/app/ui/header.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+} from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+const list = [
+  { name: "Home", href: "#home" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
+
+export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <header className="fixed w-full h-20 shadow-gray-900 shadow-2xs left-0 top-0 px-5 lg:px-28 content-center select-none">
+        <div className="text-text flex justify-between box-content">
+          <Link href="#home" className="content-center">
+            <div className="text-xl text-highlight">San Min Aung</div>
+          </Link>
+          <button
+            onClick={() => {
+              setOpen(true);
+            }}
+            title="menu_btn"
+            className="size-10 flex justify-center items-center hover:bg-white lg:hidden"
+          >
+            <Bars3Icon className="size-6" />
+          </button>
+          <ul className="hidden lg:flex space-x-7 text-lg">
+            {list.map((list) => {
+              return (
+                <Link
+                  href={list.href}
+                  key={list.name}
+                  className="content-center"
+                >
+                  <li
+                    key={list.name}
+                    tabIndex={-1}
+                    className="content-center border-b-2 border-transparent hover:border-highlight focus:border-highlight py-2"
+                  >
+                    {list.name}
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+      </header>
+      <Dialog open={open} onClose={setOpen} className="relative z-10 lg:hidden">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-500/75 transition-opacity duration-500 ease-in-out data-closed:opacity-0"
+        />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <DialogPanel
+                transition
+                className="pointer-events-auto relative w-[50vw] max-w-md transform transition duration-500 ease-in-out data-closed:translate-x-full sm:duration-700"
+              >
+                <TransitionChild>
+                  <div className="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 duration-500 ease-in-out data-closed:opacity-0 sm:-ml-10 sm:pr-4">
+                    <button
+                      type="button"
+                      onClick={() => setOpen(false)}
+                      className="relative rounded-md text-gray-300 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden"
+                    >
+                      <span className="absolute -inset-2.5" />
+                      <span className="sr-only">Close panel</span>
+                      <XMarkIcon aria-hidden="true" className="size-6" />
+                    </button>
+                  </div>
+                </TransitionChild>
+                <div className="flex h-full flex-col overflow-y-scroll bg-secondary py-6 shadow-xl w-[50vw]">
+                  <div className="px-4 sm:px-6">
+                    <DialogTitle className="text-base font-semibold text-gray-900">
+                      San Min Aung
+                    </DialogTitle>
+                  </div>
+                  <div className="relative mt-6 flex-1">
+                    <ul className="text-black text-lg">
+                      {list.map((list) => {
+                        return (
+                          <Link
+                            href={list.href}
+                            key={list.name}
+                            className="content-center"
+                          >
+                            <li
+                              key={list.name}
+                              tabIndex={-1}
+                              className="content-center border-b-2 border-transparent w-fit hover:border-highlight focus:border-highlight mx-4 py-2"
+                            >
+                              {list.name}
+                            </li>
+                          </Link>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
+              </DialogPanel>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+}
---
+++ src/app/ui/hero.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="mt-20 h-[80vh] flex flex-col lg:flex-row justify-center lg:space-x-10">
+      <div className="content-center self-center">
+        <Image
+          alt="profile"
+          src="/profile.jpg"
+          width="300"
+          height="300"
+          className="rounded-full aspect-square object-cover"
+        />
+      </div>
+      <div className="content-center text-center text w-fit self-center">
+        <p className="text-left text-gray-500">Hello&#44; I&apos;m</p>
+        <h1 className="font-bold text-5xl my-3">San Min Aung</h1>
+        <p className="font-bold text-3xl text-gray-400 mb-3">
+          Fullstack Developer
+        </p>
+        <div className="space-x-5">
+          <button
+            tabIndex={0}
+            className="border-2 rounded-2xl p-2 hover:bg-secondary hover:text-primary hover:border-2 hover:border-secondary"
+          >
+            Download CV
+          </button>
+          <button
+            tabIndex={0}
+            className="rounded-2xl p-2 bg-secondary text-primary font-bold border-2 border-secondary hover:bg-primary hover:text-secondary hover:border-secondary hover:border-2"
+          >
+            Contact Info
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
